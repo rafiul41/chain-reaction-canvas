19e1f666bacc405fc383e231f3a02f50b3cbb044
rafiul41 <rafiul1994@gmail.com>
2021-03-31 14:54:15 +0600

    Some refactoring

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 // Important variables
-const canvasHeight = 600, canvasWidth = 600;
+const marginLeft = 0, marginTop = 0;
+
+const gridStart = {
+  x: marginLeft, y: marginTop
+};
+const gridHeight = 600, gridWidth = 600;
 const row = 5, col = 5;
 
 // Cell will be square
 const cellWidth = 100, cellHeight = 100;
 
-function makeLine(start, end) {
+function drawLine(start, end) {
   ctx.beginPath();
   ctx.moveTo(start.x, start.y);
   ctx.lineTo(end.x, end.y);
@@ -16,35 +21,35 @@ function makeLine(start, end) {
 }
 
 function makeGrid() {
-  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  ctx.fillRect(0, 0, gridWidth, gridHeight);
 
 // draw vertical lines
   let start = {
-    x: 0, y: 0
+    x: gridStart.x, y: gridStart.y
   };
   let end = {
-    x: 0, y: col * cellWidth
+    x: gridStart.x, y: col * cellWidth
   };
 
   ctx.strokeStyle = 'white';
 
   for(let i = 0; i < row + 1; i++) {
-    makeLine(start, end);
+    drawLine(start, end);
     start.x += cellWidth;
     end.x += cellWidth;
   }
 
   // draw horizontal lines
   start = {
-    x: 0, y: 0
+    x: gridStart.x, y: gridStart.y
   };
 
   end = {
-    x: row * cellWidth, y: 0
+    x: row * cellWidth, y: gridStart.y
   };
 
   for(let i = 0; i < col + 1; i++) {
-    makeLine(start, end);
+    drawLine(start, end);
     start.y += cellHeight;
     end.y += cellHeight;
   }
