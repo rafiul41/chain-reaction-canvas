ab0170b8122b717162a983031e18dc06b05447d4
rafiul41 <rafiul1994@gmail.com>
2021-06-20 18:24:43 +0600

    Handling static states

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ const row = 6, col = 6;
 // Cell will be square
 const cellSize = 100;
 
+const dx = [0, 0, 1, -1];
+const dy = [1, -1, 0, 0];
+
 const ballCount = {};
+let cellsToUpdate = [];
 
 for(let i = 0; i < row; i++) {
   ballCount[i] = {};
@@ -37,6 +41,15 @@ function isCorner(r, c) {
 function isEdge(r, c) {
   return (r === 0 && c > 0 && c < col - 1) || (r === row - 1 && c > 0 && c < col - 1)
     || (c === 0 && r > 0 && r < row - 1) || (c === col - 1 && r > 0 && r < row - 1);
+}
+
+function isValidCell(r, c) {
+  return r >= 0 && r < row && c >= 0 && c < col;
+}
+
+function shouldExplode(r, c) {
+  return !!((isCorner(r, c) && ballCount[r][c] > 1) || (isEdge(r, c) && ballCount[r][c] > 2) || (ballCount[r][c] > 3));
+
 }
 
 function drawLine(start, end) {
@@ -65,57 +78,63 @@ function drawCircleInCell(r, c, color) {
     drawCircle({x: c * cellSize + cellSize / 2.8, y: r * cellSize + ((2 * cellSize) /  3)}, cellSize / 5, color);
     drawCircle({x: c * cellSize + cellSize / 1.5, y: r * cellSize + cellSize /  2}, cellSize / 5, color);
   }
-  window.requestAnimationFrame(new Function());
 }
 
-function explode(r, c) {
-  console.log('this cell should be exploded by now !!!');
+function resetCell(r, c) {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(c * cellSize, r * cellSize, cellSize, cellSize);
+  ctx.strokeStyle = 'white';
+  ctx.strokeRect(c * cellSize, r * cellSize, cellSize, cellSize);
 }
 
 function makeGrid() {
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, gridWidth, gridHeight);
-
-// draw vertical lines
-  let start = {
-    x: gridStart.x, y: gridStart.y
-  };
-  let end = {
-    x: gridStart.x, y: col * cellSize
-  };
-
-  ctx.strokeStyle = 'white';
-
-  for(let i = 0; i < row + 1; i++) {
-    drawLine(start, end);
-    start.x += cellSize;
-    end.x += cellSize;
+  for(let i = 0; i < row; i++) {
+    for(let j = 0; j < col; j++) {
+      resetCell(i, j);
+    }
   }
 
-  // draw horizontal lines
-  start = {
-    x: gridStart.x, y: gridStart.y
-  };
-
-  end = {
-    x: row * cellSize, y: gridStart.y
-  };
-
-  for(let i = 0; i < col + 1; i++) {
-    drawLine(start, end);
-    start.y += cellSize;
-    end.y += cellSize;
-  }
+  ctx.strokeRect(0, 0, gridWidth, gridHeight);
 }
 
 makeGrid();
 
-function resetAndUpdateGrid() {
-  makeGrid();
-  for(let i = 0; i < row; i++) {
-    for(let j = 0; j < col; j++) {
-      drawCircleInCell(i, j, 'red');
+function renderUpdatedCells() {
+  for(let i = 0; i < cellsToUpdate.length; i++) {
+    resetCell(cellsToUpdate[i].r, cellsToUpdate[i].c);
+    drawCircleInCell(cellsToUpdate[i].r, cellsToUpdate[i].c, 'red');
+  }
+}
+
+function updateCellsBFS(r, c) {
+  const queue = [];
+  queue.push({r, c});
+
+  while(queue.length !== 0) {
+    let levelNodes = queue.length;
+
+    cellsToUpdate = [];
+
+    for(let k = 0; k < levelNodes; k++) {
+      let front = queue[0];
+      queue.shift();
+      ballCount[front.r][front.c]++;
+
+      if(shouldExplode(front.r, front.c)) {
+        ballCount[front.r][front.c] = 0;
+
+        for(let i = 0; i < dx.length; i++) {
+          let x = front.r + dx[i];
+          let y = front.c + dy[i];
+
+          if(isValidCell(x, y)) {
+            queue.push({r: x, c: y});
+          }
+        }
+      }
+      cellsToUpdate.push({r: front.r, c: front.c});
     }
+    renderUpdatedCells();
   }
 }
 
@@ -128,8 +147,6 @@ canvas.addEventListener('click', (e) => {
   const clickedCol = Math.floor(canvasCo.x / cellSize), clickedRow = Math.floor(canvasCo.y / cellSize);
   console.log(clickedRow, clickedCol);
   if(clickedRow < row && clickedCol < col) {
-    ballCount[clickedRow][clickedCol]++;
+    updateCellsBFS(clickedRow, clickedCol);
   }
-
-  resetAndUpdateGrid();
 });
